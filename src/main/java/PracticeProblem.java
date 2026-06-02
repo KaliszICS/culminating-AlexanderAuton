03fc5ad574e1a2c6dee175d05013f236c14df0a5
AlexanderAuton <350319867@tdsb.ca>
2026-06-02 15:38:48 +0000

    V1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,16 @@
+/**
+
+        * File: Culminating
+
+        * Author: Alexander D. Auton
+
+        * Date Created: June 02, 2026
+
+        * Date Last Modified: June --, 2026
+
+        */
+	   import java.util.Scanner;
+	   //and so it begins
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 }
